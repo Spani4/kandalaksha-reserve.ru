2caf2e467c3944da150d7ddc2e12be913027c76b
Anton <youz.sani4@gmail.com>
2020-02-17 23:25:56 +0300

    collapsible nav

---
+++ src/js/components/collapsed-nav.js
@@ -0,0 +1,68 @@
+const nav = document.querySelector('.js-nav');
+const visibleNav = nav.querySelector('.js-general-nav');
+const collapsedNav = nav.querySelector('.js-collapsed-nav');
+const btn = nav.querySelector('.js-collapsed-btn');
+const items = [...collapsedNav.children];
+
+
+function popItem() {
+
+    collapsedNav.prepend(visibleNav.lastElementChild);
+}
+
+function pushItem(item = null) {
+
+    if ( item ) {
+        visibleNav.append(item);
+    } else {
+        visibleNav.append(collapsedNav.firstElementChild);
+    }
+}
+
+function adjustItems() {
+    items.forEach(item => {
+        if ( nav.offsetWidth - btn.offsetWidth - visibleNav.offsetWidth > 170) {
+            pushItem(item);
+        }
+    });
+}
+
+function adjustBtn() {
+
+    if ( collapsedNav.children.length === 0) {
+        btn.classList.add('hidden');
+    } else {
+        btn.classList.remove('hidden');
+    }
+}
+
+function checkNavContent() {
+
+    if ( nav.offsetWidth - btn.offsetWidth - visibleNav.offsetWidth <= 170) {
+
+        if ( visibleNav.children.length === 0 ) return;
+        popItem();
+        checkNavContent();
+    } else {
+        pushItem();
+    }
+
+    adjustBtn();
+}
+
+function handleResize() {
+
+    checkNavContent();
+}
+
+
+export default function() {
+
+    adjustItems();
+    adjustBtn();
+
+    window.addEventListener('resize', () => {
+        handleResize();
+    });
+
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import "../scss/main.scss";
 
+import collapsedNav from "./components/collapsed-nav";
+
 import { initMainPageMap } from "./components/maps";
 
 document.addEventListener('DOMContentLoaded', () => {
     
+    collapsedNav();
     initMainPageMap();
 
 });
