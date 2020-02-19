c38bd1cbc526ccd9015f6fca2e259cea56a0f844
anton <youz.sani4@gmail.com>
2020-02-19 14:12:58 +0300

    nav corrections

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -2,12 +2,44 @@ import "swiper/css/swiper.min.css";
 
 import Swiper from "swiper";
 
+
+function initAutoSlide(swiper, progressBar) {
+
+    console.log(swiper);
+    let progressBarWidth = 0;
+
+    setInterval(() => {
+        
+        if ( progressBarWidth === 100 ) {
+            progressBarWidth += 0.5;
+            // progressBar.style.width = `${progressBarWidth}%`;
+            console.log(progressBar);
+            swiper.slideNext();
+        }
+    }, 120);
+
+    // swiper.on('transitionStart', () => {
+    //     console.log('tr start');
+    //     progressBarWidth = 0;
+    //     progressBar.style.width = `${progressBarWidth}%`;
+    // });
+
+    // swiper.on('transitionEnd', () => {
+    //     console.log('tr end');
+    //     progressBarWidth = 0;
+
+    // });
+}
+
+
 export function initMainSlider() {
 
     const section = document.querySelector('.main-slider');
     const swiperContainer = section.querySelector('.swiper-container');
     const btnPrev = section.querySelector('.slider-btn.left');
     const btnNext = section.querySelector('.slider-btn.right');
+    const progressBar = section.querySelector('.progress-bar');
+
 
     const swiper = new Swiper(swiperContainer, {
         loop: true,
@@ -19,10 +51,16 @@ export function initMainSlider() {
             nextEl: btnNext
         },
 
-        autoplay: {
-            delay: 7000,
-        },
+        // init: false,
     });
+
+    // swiper.on('init', () => {
+    //     initAutoSlide(swiper, progressBar);
+    // });
+
+    // swiper.init();
+
+
 }
 
 export function initTerritorySlider() {
